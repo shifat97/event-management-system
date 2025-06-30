625f17e2159dd624bdda2dfcdd9d9c607f5befb7
shifat97 <shifatbinreza2@gmail.com>
2025-06-30 18:29:54 +0600

    link pages to nav items and add home page

--- event_management_system/urls.py
+++ event_management_system/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from home.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
 ]
